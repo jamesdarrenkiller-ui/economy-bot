e5e0c6b2cb71cb970c55245e5ab414d62eaa5acf
James <james.darren.killer@gmail.com>
2026-08-28 13:04:02 +0530

    refactor: make index a thin application entrypoint

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { config, validateConfig } = require('./config/config');
-const { initDatabase } = require('./database/database');
+const { connectDatabase } = require('./database/database');
 const { registerCommands } = require('./handlers/commandHandler');
 const { handleInteraction } = require('./handlers/interactionHandler');
 const { handleError } = require('./handlers/errorHandler');
 
 validateConfig();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.once('ready', async () => {
-  await initDatabase();
-  await registerCommands(config);
+  await connectDatabase();
+  await registerCommands(client);
   console.log(`Logged in as ${client.user.tag}`);
+  console.log(`Currency: ${config.currencySymbol} ${config.currencyName}`);
 });
-
-client.on('interactionCreate', (interaction) => handleInteraction(interaction));
+client.on('interactionCreate', interaction => handleInteraction(interaction).catch(handleError));
 client.on('error', handleError);
 process.on('unhandledRejection', handleError);
 process.on('uncaughtException', handleError);
-
 client.login(config.token);
