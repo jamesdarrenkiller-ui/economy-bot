6e440f8978d43957cbc9bf2872d48807397bf9d3
James <james.darren.killer@gmail.com>
2026-08-28 13:05:08 +0530

    handlers: register modular commands

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const commands = require('../commands');
 const { config } = require('../config/config');
+const commands = require('../commands');
 async function registerCommands(){const rest=new REST({version:'10'}).setToken(config.token);const route=config.guildId?Routes.applicationGuildCommands(config.clientId,config.guildId):Routes.applicationCommands(config.clientId);await rest.put(route,{body:commands.map(c=>c.data.toJSON())});console.log(`Registered ${commands.length} commands`);}
 module.exports={registerCommands};
