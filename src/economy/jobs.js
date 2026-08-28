d2e64d43c67e666fdde57d63ab14049822637273
James <james.darren.killer@gmail.com>
2026-08-28 13:05:58 +0530

    economy: add jobs service

---
+++ src/economy/jobs.js
@@ -0,0 +1,2 @@
+const JOBS={worker:{name:'Worker',min:80,max:140,cooldown:2700000},developer:{name:'Developer',min:120,max:220,cooldown:3600000},designer:{name:'Designer',min:100,max:190,cooldown:3000000},trader:{name:'Trader',min:90,max:260,cooldown:3300000}};
+module.exports={JOBS};
